5e24f6b068ca730681329597b683869737afa003
Christian Guevara <chrisdb150@gmail.com>
2022-08-09 09:50:09 -0400

    creating themes and ui in the project

---
+++ components/layouts/Layout.tsx
@@ -0,0 +1,25 @@
+import { Box } from "@mui/material"
+import Head from "next/head"
+import { FC, PropsWithChildren } from "react";
+import { NavBar } from "../ui";
+
+interface Props extends PropsWithChildren {
+    title?: string;
+}
+
+export const Layout: FC<Props> = ({ title = 'OpenJira', children }) => {
+    return (
+        <Box sx={ { flexFlow: 1 } }>
+            <Head>
+                <title>{ title }</title>
+            </Head>
+
+            <NavBar />
+            {/* SideBar */ }
+
+            <Box sx={ { padding: '10px 20px' } }>
+                { children }
+            </Box>
+        </Box>
+    )
+}
